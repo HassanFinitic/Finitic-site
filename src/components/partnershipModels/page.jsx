b83d4e168b92c6c709d47ba696b9a7e561afaa66
Dina Abdullah <dinaabdallah21244@gmail.com>
2025-02-04 01:19:02 +0200

    feat: add partnership

--- src/components/partnershipModels/page.jsx
+++ src/components/partnershipModels/page.jsx
@@ -8,15 +8,27 @@ const PartnershipModels = () => {
             <button type="button">
             Agency Model
             </button>
+            <p>
+            Collaborate as an agent by introducing clients to FINITIC and earn competitive commissions. This model fosters mutual growth in the forex trading industry and rewards your network-building efforts.
+            </p>
             <button type="button"> 
             Product Integration
             </button>
+            <p>
+            Integrate your technology or services with FINITIC’s ecosystem to offer comprehensive solutions to forex brokers. Together, we can provide seamless experiences that drive success in currency exchange.
+            </p>
             <button type="button">
             Affiliate Referrals
             </button>
+            <p>
+            Refer potential clients to FINITIC and earn attractive referral commissions. Share our forex trading strategies and innovative solutions with your network and watch your efforts translate into real rewards. 
+            </p>
             <button type="button">
             Reseller Opportunities
             </button>
+            <p>
+            Become a reseller of FINITIC’s cutting-edge forex CRM software and services. Strengthen your portfolio by offering proven forex trading CRM solutions while leveraging our global reputation. 
+            </p>
         </div>
         </div>
     </div>
