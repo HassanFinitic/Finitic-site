97a27d0aafe69c4a9244ff77d78e1cb114a6d4c7
HassanFinitic <A.Hassan@finitic.com>
2025-02-03 15:16:44 +0200

    handle the container

--- src/components/heroSection/Hero.jsx
+++ src/components/heroSection/Hero.jsx
@@ -5,7 +5,7 @@ import banner from "../../../public/assets/images/banner.jpg"
 
 export default function Hero() {
   return (
-    <div className={style["full-hero-container"]}>
+    <div className={`container ${style["full-hero-container"]}`}>
         <div className={ `container ${style["hero-container"]}`}>  
             <Image src={banner} alt="banner Image" />
         </div>
