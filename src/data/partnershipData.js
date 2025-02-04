4238374a9fff1a5d27a636dead77e43ba6c96291
dina-abdullahh <Dina@finitic.com>
2025-02-04 14:57:12 +0200

    feat: add partnership page

---
+++ src/data/partnershipData.js
@@ -0,0 +1,26 @@
+export const partnershipModels = [
+    {
+      title: "Agency Model",
+      description:
+        "Collaborate as an agent by introducing clients to FINITIC and earn competitive commissions. This model fosters mutual growth in the forex trading industry and rewards your network-building efforts.",
+      image: "/assets/images/partner1.jpg",
+    },
+    {
+      title: "Product Integration",
+      description:
+        "Integrate your technology or services with FINITIC’s ecosystem to offer comprehensive solutions to forex brokers. Together, we can provide seamless experiences that drive success in currency exchange.",
+        image: "/assets/images/partner2.jpg",
+    },
+    {
+      title: "Affiliate Referrals",
+      description:
+        "Refer potential clients to FINITIC and earn attractive referral commissions. Share our forex trading strategies and innovative solutions with your network and watch your efforts translate into real rewards.",
+        image: "/assets/images/partner3.jpg",
+    },
+    {
+      title: "Reseller Opportunities",
+      description:
+        "Become a reseller of FINITIC’s cutting-edge forex CRM software and services. Strengthen your portfolio by offering proven forex trading CRM solutions while leveraging our global reputation.",
+        image: "/assets/images/partner4.jpg",
+    },
+  ];
