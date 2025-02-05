60532340c1764351ea2501d1d72790e63f856156
HassanFinitic <A.Hassan@finitic.com>
2025-02-05 10:57:17 +0200

    handle the basic of customized grid section

--- src/components/customGrid/CustomGrid.jsx
+++ src/components/customGrid/CustomGrid.jsx
@@ -1,31 +1,42 @@
 import React from 'react';
-import styles from './CustomGrid.module.css'; // Assuming you're using SCSS for styling
+import styles from './CustomGrid.module.css'; 
+import Flex from '../flex/Flex';
+import Image from 'next/image';
 
 const CustomGrid = ({ cards }) => {
+
   return (
     <div className={styles.grid}>
       {cards.map((card, index) => (
         <div
+          data-aos="zoom-in-up"
           key={card.id}
           className={styles.card}
           style={{
             gridColumn:
               index % 4 === 0 // First card in every 4-card cycle (rows)
-                ? 'span 2' // Double width
+                ? 'span 3' // Double width
                 : index % 4 === 1 // Second card in every 4-card cycle
-                ? 'span 1' // Normal width
+                ? 'span 2' // Normal width
                 : index % 4 === 2 // Third card in every 4-card cycle
-                ? 'span 1' // Normal width
-                : 'span 2', // Fourth card in every 4-card cycle (double width)
+                ? 'span 2' // Normal width
+                : 'span 3', // Fourth card in every 4-card cycle (double width)
           }}
         >
+          <Flex direction="column" gap="50px">
             <div className={styles.heading}>
+              <Flex direction="row" justify="space-between" align="center">
                 <div className={styles.title}>{card.title}</div>
-                <div className={styles.icon}></div>
+                <div className={styles.icon}>
+                <Image src={card.icon} width={25} height={25} alt='icon'></Image>
+                </div>
+
+              </Flex>
             </div>
             <div className={styles.content}>
                 {card.content}
             </div>
+        </Flex>
         </div>
       ))}
     </div>
