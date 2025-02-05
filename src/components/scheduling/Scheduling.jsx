e6cf04757cad4aaa22d4fed7881f5168dae38b11
HassanFinitic <A.Hassan@finitic.com>
2025-02-05 11:47:15 +0200

    handle the form

--- src/components/scheduling/Scheduling.jsx
+++ src/components/scheduling/Scheduling.jsx
@@ -3,8 +3,9 @@ import React  from "react";
 import Input from "../shared/input/Input";
 import style from "./scheduling.module.css";
 import Submit from "../shared/submit/Submit";
+import Link from "next/link";
 
-const Scheduling = () => {
+const Scheduling = ({ subTitle, buttonTitle }) => {
     const [formData, setFormData] = React.useState({
         name: "",
         number: "",
@@ -13,7 +14,7 @@ const Scheduling = () => {
       });
     
      const [loading, setLoading] = React.useState(false);
-     const [submitTitle, setSubmitTitle] = React.useState("Sumbit");
+     const [submitTitle, setSubmitTitle] = React.useState(buttonTitle || "Submit");
       const handleChange = (e) => {
         console.log(e.target.name, e.target.value);
         const { name, value } = e.target;
@@ -38,9 +39,10 @@ const Scheduling = () => {
   return (
     <div className={style["scheduling-container"]}> 
          <h3 className={style["title"]}>Schedule Your Demo  Now</h3>
+         <p className={style["p"]}>{subTitle}</p>
           <form className={style["form"]} onSubmit={handleSubmit}>
             <Input formData={formData} handleChange={handleChange} type="text" name="name" placeholder="Enter Your Name " />
-            <Input formData={formData} handleChange={handleChange} type="number" name="number" placeholder="Enter Your Phone " />
+            <Input formData={formData} handleChange={handleChange} type="text" name="number" placeholder="Enter Your Phone " />
             <Input  formData={formData} handleChange={handleChange} type="email" name="email" placeholder="Enter Your Email  " />
             <select required  onChange={handleChange} className={style["select"]}  name="jobTitle" id="jobTitle">
               <option value="">Select Your Job Title</option>
@@ -49,6 +51,33 @@ const Scheduling = () => {
               <option value="Trader">Trader</option>
               <option value="Other">Other</option>
             </select>
+
+            {/* <select defaultValue={"Starter"} required  onChange={handleChange} className={style["select"]}  name="plan" id="plan">
+              <option value="">Select Your Plan</option>
+              <option value="Manager">Starter</option>
+              <option value="Broker">Premium</option>
+              <option value="Trader">Enterprise</option>
+              <Link href={"/"}>more about plans</Link>
+            </select> */}
+
+            {/* <div> */}
+      <select
+        defaultValue={"starter"} // Match the value of the selected option
+        required
+        onChange={handleChange}
+        className={style.select}
+        name="plan"
+        id="plan"
+      >
+        <option value="">Select Your Plan</option>
+        <option value="Starter">Starter</option>
+        <option value="Premium">Premium</option>
+        <option value="Enterprise">Enterprise</option>
+      </select>
+      <Link href="/pricing" className={style.link}>
+        More about plans ?
+      </Link>
+    {/* </div> */}
             <p className={style["p"]}>By clicking the button below, you agree to our Terms and have read our Privacy Policy</p>
             <Submit loading={loading}  type="submit">{submitTitle}</Submit>
           </form>
