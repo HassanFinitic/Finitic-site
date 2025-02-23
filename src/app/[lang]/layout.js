fcd61712b65a53bc0bfc0d30d9e8690f97ad7234
HassanFinitic <a.hassan@finitic.com>
2025-02-23 23:02:05 +0200

    remove the lang files

--- src/app/[lang]/layout.js
+++
@@ -1,9 +0,0 @@
-export default function LangLayout({ children, params }) {
-    const { lang } = params;
-  
-    return (
-      <html dir={lang === 'ar' ? 'rtl' : 'ltr'} lang={lang}>
-        <body>{children}</body>
-      </html>
-    );
-  }
