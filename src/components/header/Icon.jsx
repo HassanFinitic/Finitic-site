3536861b4a387a386d55fa442b9d430cfad0301e
HassanFinitic <a.hassan@finitic.com>
2025-02-24 09:41:44 +0200

    use the react icons insted of the icon of the header

--- src/components/header/Icon.jsx
+++ src/components/header/Icon.jsx
@@ -1,16 +1,21 @@
 
 import React from 'react'
+import { FaBars } from "react-icons/fa6";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 export default function Icon({toggleIcon, isOpen, style}) {
   console.log(style)
     return (
     <div className={style["icon"]} onClick={toggleIcon}>
-        <div
+        {/* <div
           className={`${style["line_one"]} ${isOpen ? style["active"] : ""}`}
         ></div>
         <div
           className={`${style["line_two"]} ${isOpen ? style["active"] : ""}`}
-        ></div>
+        ></div> */}
+        {
+          isOpen ? <FaBarsStaggered /> : <FaBars />
+        }
       </div>
   )
 }
