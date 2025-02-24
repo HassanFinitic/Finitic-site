b14e339a3453b1ff17034834338f6dad7cd7beb7
HassanFinitic <a.hassan@finitic.com>
2025-02-24 10:04:54 +0200

    roleback to the last version of the icon by css

--- src/components/header/Icon.jsx
+++ src/components/header/Icon.jsx
@@ -7,15 +7,15 @@ export default function Icon({toggleIcon, isOpen, style}) {
   console.log(style)
     return (
     <div className={style["icon"]} onClick={toggleIcon}>
-        {/* <div
+        <div
           className={`${style["line_one"]} ${isOpen ? style["active"] : ""}`}
         ></div>
         <div
           className={`${style["line_two"]} ${isOpen ? style["active"] : ""}`}
-        ></div> */}
-        {
+        ></div>
+        {/* {
           isOpen ? <FaBarsStaggered /> : <FaBars />
-        }
+        } */}
       </div>
   )
 }
