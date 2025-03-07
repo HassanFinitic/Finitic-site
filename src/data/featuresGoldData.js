b02fc976fd1b1f6082167cc8498ba6a713b651eb
HassanFinitic <a.hassan@finitic.com>
2025-03-07 13:14:42 +0200

    handle gold page with new svgs

--- src/data/featuresGoldData.js
+++ src/data/featuresGoldData.js
@@ -3,20 +3,20 @@ export const featuresGoldData = [
         title: "Flexible Trading Options",
         description:
           "Facilitate spot trading, physical settlement, and leverage trading to accommodate various investment strategies.",
-    image: "/assets/images/gold1.jpg",
+    image: "/assets/images/gold1.svg",
       },
       {
         title: "24/7 Support",
         description:
           "Our expert team is available around the clock to assist with trading operations, technical issues, and market insights.",
-          image: "/assets/images/gold2.jpg",
+          image: "/assets/images/gold2.svg",
 
       },
       {
         title: "Asset Integration",
         description:
           "Integrate gold trading with forex, cryptocurrencies, and other financial instruments to provide a holistic investment platform.",
-          image: "/assets/images/gold3.jpg",
+          image: "/assets/images/gold3.svg",
 
       },
 ]
