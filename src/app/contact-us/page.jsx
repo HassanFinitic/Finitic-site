b001a12f08008fda83372418f02d3e7cfa545388
HassanFinitic <a.hassan@finitic.com>
2025-03-10 10:23:56 +0200

    handle the meta data for the ontact us

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -6,9 +6,9 @@ import Email from "../../../public/assets/icons/Email";
 import Submit from "@/components/shared/submit/Submit";
 
 export const metadata = {
-  title: "Forex FAQ | Frequently Asked Questions | Finitic Technologies ",
+  title: "Forex Contact Us | Contact Us | Finitic Technologies ",
   description:"Find answers to common questions about forex trading, account management, trading platforms, and more in our comprehensive Forex FAQ section.",
-  keywords: ["Forex FAQ", "frequently asked questions", "forex trading", "account management", "trading platforms"],
+  keywords: ["Forex Contact Us", "frequently asked questions", "forex trading", "account management", "trading platforms"],
 };
 
 const page = () => {
