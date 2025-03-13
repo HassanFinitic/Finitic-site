332c3c4323d056ed58a4656ae792de0f93781e9c
HassanFinitic <a.hassan@finitic.com>
2025-03-13 11:45:07 +0200

    handle the basic new design of data tools

--- src/app/products/data-tools/page.jsx
+++ src/app/products/data-tools/page.jsx
@@ -1,9 +1,46 @@
-import DataAccessOptions from '@/components/dataAccessOptions/DataAccessOptions'
+import Header from "@/components/shared/header/Header";
+import DataAccessOptions from "@/components/dataAccessOptions/DataAccessOptions";
+import Heading from "@/components/productsRoute/shared/Heading";
+import Grid from "@/components/grid/Grid";
+import Button from "@/components/shared/button/Button";
+import Flex from "@/components/flex/Flex";
+import Subscribe from "@/components/subscribe/Subscribe";
 
 export default function page() {
   return (
     <div>
-        <DataAccessOptions />
+      <Header
+        title="Advanced Market Data Tools"
+        decription="Real-Time Market Data APIs"
+        fontSizeDescription={"3rem"}
+      />
+      <Header
+        title="Forex, Commodities, and Crypto"
+        decription="Ultra-fast, highly accurate, and seamlessly integrated for uninterrupted trading."
+        fontSizeDescription={"1rem"}
+      />
+
+
+      <Flex gap="40px" align="center" justify="center" >
+        <Button background={"black"} text="Get your Free API Key" url="/registration" />
+        <Button text="View Pricing" url="/registration" />
+      </Flex>
+
+      
+      <br />
+      <br />
+      <br />
+
+      <Header
+        title="Real-time & Historical"
+        decription="Extensive Market Data APIs"
+        fontSizeDescription={"1.5rem"}
+      />
+
+      <DataAccessOptions />
+
+      <Subscribe />
+
     </div>
-  )
+  );
 }
