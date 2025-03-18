786b6def6c79b35fbfb28ea2885b53a66664927b
HassanFinitic <a.hassan@finitic.com>
2025-03-18 14:13:50 +0200

    handle the basic of home

--- src/components/howItWorks/HowItWorks.jsx
+++ src/components/howItWorks/HowItWorks.jsx
@@ -75,8 +75,8 @@ const HowItWorks = () => {
             data-aos-easing="ease-out-cubic"
             data-aos-duration={item?.delay}>
               <div className={style["how-it-works__item-icon"]}>
-                <Image width={50} height={50} src={item.image} alt={item.imagealt} />
-                <h3  className={style["how-it-works__item-title"]}>{item.title}</h3>
+                <Image width={40} height={40} src={item.image} alt={item.imagealt} />
+                <h6 className={style["how-it-works__item-title"]}>{item.title}</h6>
               </div>
               
               <p className={style["how-it-works__item-description"]}>{item.description}</p>
