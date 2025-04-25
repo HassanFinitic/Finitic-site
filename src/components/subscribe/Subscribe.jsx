c4e26e379f3e5882c94a296d37b875919465d433
HassanFinitic <a.hassan@finitic.com>
2025-04-25 12:28:24 +0300

    handle the language in testimoies and subscribe

--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -3,7 +3,9 @@ import { useEffect, useRef, useState } from "react";
 import Qoute from "../../../public/assets/icons/Qoute";
 import style from "./subscribe.module.css";
 import Aos from "aos";
+import { useTranslations } from "next-intl";
 const Subscribe = () => {
+    const t = useTranslations();
     const [email, setEmail] = useState("")
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -21,11 +23,11 @@ const Subscribe = () => {
         <div className={`container  ${style["subscribe"]}`}>
             <div className={style["subscribe-container"]} data-aos="zoom-in-up">
                 <Qoute />
-                <h3>Stay Ahead in Forex Innovation</h3>
-                <p>Get ready to streamline operations, enhance trading strategies, and elevate your brokerage—all with FINITIC’s cutting-edge solutions. Subscribe now and stay informed.</p>
+                <h3>{t("Stay Ahead in Forex Innovation")}</h3>
+                <p>{t("Get ready to streamline operations, enhance trading strategies, and elevate your brokerage—all with FINITIC’s cutting-edge solutions Subscribe now and stay informed")}</p>
                 <div className={style["subscribe-form"]}>
-                    <input id="subscribe-input" value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" required placeholder="Enter your email address" />
-                    <button className={style["absolute-btn"]} type="submit" onClick={handleSubmit}>Subscribe</button>
+                    <input id="subscribe-input" value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" required placeholder={t("Enter your email address")} />
+                    <button className={style["absolute-btn"]} type="submit" onClick={handleSubmit}>{t("Subscribe")}</button>
                 </div>
             </div>
         </div>
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,8 +5,9 @@ import { useEffect } from "react"
 import Aos from "aos";
 import style from "./testimonials.module.css"
 import TestimonialGrid from "./TestimonialGrid";
-
+import { useTranslations } from "next-intl";
 const Testimonials = () => {
+    const t = useTranslations();
     useEffect(() => {
         Aos.init({
             duration: 500,
@@ -15,11 +16,11 @@ const Testimonials = () => {
     }, []);
     return (
         <div className={`container ${style["testimonials-container"]}`}>
-            <Header title={"Testimonials"} decription={"Trusted by 14,000+ users"} />
+            <Header title={t("Testimonials")} decription={t("Trusted by 14,000+ users")} />
             <TestimonialGrid />
             <div className="overlay"></div>
             <div>
-                <Button url={"/reviews"} text={"See All Reviews"} center={true} background={"black"} />
+                <Button url={"/reviews"} text={t("See All Reviews")} center={true} background={"black"} />
             </div>
         </div>
     )
