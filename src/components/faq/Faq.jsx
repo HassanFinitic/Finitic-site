eebd94f4920c7bf412259e4627b8f9b938b6a2e0
HassanFinitic <a.hassan@finitic.com>
2025-04-25 14:24:28 +0300

    handle the language in faqs

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -4,21 +4,23 @@ import Header from "../shared/header/Header";
 import { faqData } from "@/data/faqData";
 import Button from "../shared/button/Button";
 import AccordionItem from "./AccordionItem";
+import { useTranslations } from "next-intl";
 
 export const metadata = {
     title: "FAQs - Finitic Technology",
   };
 
 const Faq = ({landingPage,description}) => {
+    const t = useTranslations();
     return (
         <div className={`  container ${styles["faq-container"]}`}>
-            <Header title={"FAQs"} decription={ description||"Find Answers to Your Queries"} />
+            <Header title={t("FAQs")} decription={ description||t("Find Answers to Your Queries")} />
             <div className="container">
                 <div className={styles.accordion}>
                     {faqData.map((item, index) => (
-                        <AccordionItem key={index} title={item.title}>
-                            <p>{item.content}</p>
-                            <p className={styles.sub}>{item.sub}</p>
+                        <AccordionItem key={index} title={t(item.title)}>
+                            <p>{t(item.content)}</p>
+                            {item.sub && <p className={styles.sub}>{t(item.sub)}</p>}
                         </AccordionItem>
                     ))}
                 
