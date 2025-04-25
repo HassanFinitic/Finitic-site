3eda02f7533a42bb32dbe2d119a99eba343fe64e
HassanFinitic <a.hassan@finitic.com>
2025-04-25 12:16:34 +0300

    handle the language of how it work

--- src/components/howItWorks/HowItWorks.jsx
+++ src/components/howItWorks/HowItWorks.jsx
@@ -1,73 +1,24 @@
 "use client"
-import User from "../../../public/assets/icons/User"
 import Header from "../shared/header/Header"
 import style from "./howItWorks.module.css"
-import Keyboard from "../../../public/assets/icons/Keyboard"
-import Guide from "../../../public/assets/icons/Guide"
 import { howItWorksData } from "@/data/howItWorksData"
 import Image from "next/image"
-import Link from "next/link"
-import { useRef, useEffect } from "react"
+import { useEffect } from "react"
 import Aos from "aos"
 import Grid from "../grid/Grid"
+import { useTranslations } from "next-intl"
 const HowItWorks = () => {
-  const sectionRefs = useRef([]);
-  // const scroll2El = (elID) => {
-  //   const element = document.getElementById(elID);
-  //   if (element) {
-  //     window.scrollTo({
-  //       top: element.offsetTop - 60,
-  //       behavior: 'smooth',
-  //     });
-  //   } else {
-  //     console.error(`Element with ID "${elID}" not found.`);
-  //   }
-  // };
-  // const onBtnClick = (e) => {
-  //   e.preventDefault();
-  //   console.log(e.target, "togo");
-  //   const goto = e.target.getAttribute('goto');
-  //   setTimeout(() => {
-  //     scroll2El(goto);
-  //   }, 100);
-  // }
-
+  const t = useTranslations();
   useEffect(() => {
     Aos.init({
       duration: 500,
       once: true
   });
-    // const sections = sectionRefs.current;
-    // const options = {
-    //   root: null,
-    //   threshold: 1,
-    // };
-    
-    // const observer = new IntersectionObserver((entries) => {
-    //   entries.forEach((entry) => {
-    //     const iconId = `icon-${entry.target.id}`;
-    //     if (entry.isIntersecting) {
-    //       document.querySelectorAll(".icon").forEach((icon) => {
-    //         if (icon.id === iconId ||
-    //           (entry.target.id === "two" && icon.id === "icon-one") ||
-    //           (entry.target.id === "three" && ["icon-one", "icon-two"].includes(icon.id))) {
-    //           icon.classList.add("active");
-    //         }
-    //       });
-    //     } else {
-    //       document.getElementById(iconId)?.classList.remove("active");
-    //     }
-    //   });
-    // }, options);
-    // sections.forEach((section) => observer.observe(section));
-    // return () => {
-    //   observer.disconnect();
-    // };
   }, []);
  
   return (
     <div className={ `container ${style["how-it-works__container"]}`}>
-      <Header title="How it works" decription="Customizable, efficient Forex trading CRM solutions." />
+      <Header title={t("How it works")} decription={t("Customizable, efficient Forex trading CRM solutions")} />
       <Grid colsLarge={3} colsMedium={2} colsSmall={1} gap="20px">
         {
           howItWorksData.map((item, index) => (
@@ -76,10 +27,10 @@ const HowItWorks = () => {
             data-aos-duration={item?.delay}>
               <div className={style["how-it-works__item-icon"]}>
                 <Image width={40} height={40} src={item.image} alt={item.imagealt} />
-                <h6  className={style["how-it-works__item-title"]}>{item.title}</h6>
+                <h6  className={style["how-it-works__item-title"]}>{t(item.title)}</h6>
               </div>
               
-              <p className={style["how-it-works__item-description"]}>{item.description}</p>
+              <p className={style["how-it-works__item-description"]}>{t(item.description)}</p>
             </div>
           ))
         }
--- src/data/howItWorksData.js
+++ src/data/howItWorksData.js
@@ -5,7 +5,7 @@ export const howItWorksData = [
       link: "one",
       title: "Tailored Integration",
       description:
-        "We integrate effortlessly with your existing systems, such as Forex trading platforms like MT4 and MT5, allowing for quick setup and minimal disruption to your operations.",
+        "We integrate effortlessly with your existing systems, such as Forex trading platforms like MT4 and MT5, allowing for quick setup and minimal disruption to your operations",
       image: "/assets/images/how-it-works1.svg",
       imagealt: "forex trading integration systems",
       duration:500
@@ -15,7 +15,7 @@ export const howItWorksData = [
       link: "two",
       title: "Scalable Solutions",
       description:
-        "Our platform grows with your business, offering customizable features that adapt to your needs, whether you're starting small or scaling globally in the forex market.",
+        "Our platform grows with your business, offering customizable features that adapt to your needs, whether you're starting small or scaling globally in the forex market",
         image: "/assets/images/how-it-works2.svg",
         imagealt: "grow your business with integrated forex solutions",
         duration:500
@@ -25,7 +25,7 @@ export const howItWorksData = [
       link: "three",
       title: "Expert Guidance Every Step of the Way",
       description:
-        "Enjoy 24/7 support and regular updates to ensure your systems, like our Forex CRM software, stay secure and efficient.",
+        "Enjoy 24/7 support and regular updates to ensure your systems, like our Forex CRM software, stay secure and efficient",
         image: "/assets/images/how-it-works3.svg",
         imagealt: "financial support for your forex business",
         duration:500
