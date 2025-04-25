ad19d7335d25991781c13775cd5c412559796dd0
HassanFinitic <a.hassan@finitic.com>
2025-04-25 10:42:07 +0300

    handle the translation in confidence section

--- src/components/confidence/Confidence.jsx
+++ src/components/confidence/Confidence.jsx
@@ -5,8 +5,9 @@ import React, { useEffect } from "react";
 import Aos from "aos";
 import Grid from "../grid/Grid";
 import Image from "next/image";
-
+import { useTranslations } from "next-intl";
 const Confidence = ({Data,title,decription}) => {
+    const t = useTranslations();
     useEffect(() => {
         Aos.init({
           duration: 1000, 
@@ -15,8 +16,8 @@ const Confidence = ({Data,title,decription}) => {
       }, []);
   return (
     <div className={` container ${styles["confidence-container"]}`}>
-        <Header  decription={title || "We Ensure Your Business Operates with Confidence"} />
-        {decription && <p className={styles["confidence-decription"]}>Expand your portfolio with FINITIC’s gold trading platform and provide your clients with trusted solutions to diversify investments in precious metals trading.</p>}
+        <Header  decription={t(title) || t("We Ensure Your Business Operates with Confidence")} />
+        {decription && <p className={styles["confidence-decription"]}>{t("Expand your portfolio with FINITIC’s gold trading platform and provide your clients with trusted solutions to diversify investments in precious metals trading.")}</p>}
         <Grid colsLarge={3} colsMedium={2} colsSmall={1} gap={"10"}>
             {Data.map((product,index) => (
                 <div key={index} className={styles["grid-item"]} data-aos="flip-up"
@@ -25,8 +26,8 @@ const Confidence = ({Data,title,decription}) => {
                     <div className={styles["image-container"]}> 
                       <Image width={50}  height={50} alt={product.imagealt || product.title} src={product.image} />
                     </div>
-                    <h6>{product.title}</h6>
-                    <p>{product.description}</p>
+                    <h6>{t(product.title)}</h6>
+                    <p>{t(product.description)}</p>
                 </div>
             ))}
         </Grid>
--- src/data/confidenceData.js
+++ src/data/confidenceData.js
@@ -4,7 +4,7 @@ export const confidenceData = [
         imagealt: "Customize your software to match your forex needs",
         title: "Customizable Solutions",
         description:
-            "Adapt our platform to meet your unique brokerage needs and growth goals.",
+            "Adapt our platform to meet your unique brokerage needs and growth goals",
         delay: "0"
     },
     {
@@ -12,7 +12,7 @@ export const confidenceData = [
         imagealt: "daily live forex trading reports",
         title: "Real-Time Data",
         description:
-            "Access live Forex trading insights for quick decisions.",
+            "Access live Forex trading insights for quick decisions",
         delay: "500"
     },
     {
@@ -20,7 +20,7 @@ export const confidenceData = [
         imagealt: "detailed fintech reporting",
         title: "Automated Reporting",
         description:
-            "Simplify operations with detailed, real-time reports for smarter decision-making.",
+            "Simplify operations with detailed, real-time reports for smarter decision-making",
         delay: "1000"
     },
 
