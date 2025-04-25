cf7059120c7418c3d05792ebdd7c2a83dbd99dbe
HassanFinitic <a.hassan@finitic.com>
2025-04-25 11:28:37 +0300

    handle the pricing section with the new languages

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -8,8 +8,10 @@ import Check from "../../../public/assets/icons/Check"
 import Button from "../header/Button"
 import Aos from "aos"
 import Link from "next/link"
+import { useTranslations } from "next-intl"
 
 const Pricing = () => {
+    const t = useTranslations();
     const [value, setValue] = useState(false)
      useEffect(() => {
           Aos.init({
@@ -19,20 +21,20 @@ const Pricing = () => {
         }, []);
     return (
         <div className={` container pricing-container`} >
-            <Header title="Plans" decription="Choose your plan " />
+            <Header title={t("Plans")} decription="Choose your plan " />
             <div className={style.pricingContainer} data-aos="zoom-in-up">
             {pricingData.map((plan, index) => (
                 <div key={index} className={index === 1 ? `${style["pricingCard"]} ${style["active"]}` : `${style["pricingCard"]}`}>
-                    <h6 className={style.planTitle}>{plan.icon}{plan.title} </h6>
+                    <h6 className={style.planTitle}>{plan.icon}{t(plan.title)} </h6>
                     <ul className={style.featuresList}>
                         {plan.features.map((feature, idx) => (
                             <li key={idx} className={style.featureItem}>
-                              <div> <Check /> </div>{feature}
+                              <div> <Check /> </div>{t(feature)}
                             </li>
                         ))}
                     </ul>
                     <div className={`${style["button-container"]}`}>
-                       <Link className={style.link} href={"/registration"} >Get Started</Link>
+                       <Link className={style.link} href={"/registration"} >{t("Get Started")}</Link>
                     </div>
                 </div>
             ))}
